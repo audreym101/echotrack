ff678b95b658fbdc1f520f86baeb01418977a99f
audreym101 <m.audrey1@alustudent.com>
2025-07-29 13:11:14 +0200

    fix: use Render port environment variable

--- server.js
+++ server.js
@@ -72,8 +72,9 @@ app.use('*', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server is live!`);
+
 });
 
